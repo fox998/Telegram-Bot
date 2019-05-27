0d930b31ea1e0c43fe9506719cb489a82cfe5397
fox998 <andrikred@gmail.com>
2019-05-27 19:23:43 +0300

    update /help

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ bot = telebot.TeleBot("896283170:AAEvIuBZNbjtqAnDC8B3ZBgJtuMP5V3AL0g")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Send me name of your groupe and get your timetable \
+        for todady. For example тк-71 ")
 
 
 @bot.message_handler(func=lambda message: True)
